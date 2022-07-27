972063eddc508c39e7f5ec5f97d0d5df38be7b79
Ana Rotariu <anarotariu@192-168-0-101.rdsnet.ro>
2022-07-27 15:29:29 +0300

    code cleanup

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const imagesContainer = document.querySelector("#imgs");
 const prevBtn = document.querySelector("#left");
 const nextBtn = document.querySelector("#right");
 const imageList = document.querySelectorAll("#imgs img");
 let index = 0;
-console.log("Image list[0] = ", imageList[index].style);
 
-// change the index of the picture and translate value for the next image
+// change the index of the picture and display only the image with the specific index
 
 function switchImage() {
   if (index > imageList.length - 1) {
