bcb8e7f7b41b1f3e3a0d62e8e753c20839e894e4
Ana Rotariu <anarotariu@192-168-0-101.rdsnet.ro>
2022-07-27 13:14:08 +0300

    final layout + carousel functionality started

---
+++ script.js
@@ -0,0 +1,5 @@
+const imagesContainer = document.querySelector("#imgs");
+const prevBtn = document.querySelector("#left");
+const nextBtn = document.querySelector("#right");
+const imageList = document.querySelectorAll("#imgs img");
+
