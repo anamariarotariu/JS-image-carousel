3a9994a445ebbdf8cd4b7e0db57c3a00f417b0cf
Ana Rotariu <anarotariu@192-168-0-101.rdsnet.ro>
2022-07-28 12:17:13 +0300

    added another way to do the slider

---
+++ script2.js
@@ -0,0 +1,28 @@
+const imagesContainer = document.querySelector(".image--container");
+const prevBtn = document.querySelector("#left");
+const nextBtn = document.querySelector("#right");
+const imageList = document.querySelectorAll(".image--container img");
+let index = 0;
+
+// change the index of the picture and translate value for the next image
+
+function switchImage() {
+  if (index > imageList.length - 1) {
+    index = 0;
+    imagesContainer.style.transition = "transform 0.1s ease-in-out";
+  } else if (index < 0) {
+    index = imageList.length - 1;
+  }
+  imagesContainer.style.transform = `translateX(${-index * 500}px)`;
+}
+
+// event listener on buttons
+
+prevBtn.addEventListener("click", () => {
+  index--;
+  switchImage();
+});
+nextBtn.addEventListener("click", () => {
+  index++;
+  switchImage();
+});
